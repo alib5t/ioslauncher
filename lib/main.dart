df7aed9b4d67ba3108cdb4f88a32716307475ac9
alib5t <alimehel157@gmail.com>
2026-06-06 21:50:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,18 @@
-package com.example.ioslauncher
+import 'package:flutter/material.dart';
+import 'home.dart';
 
-import android.content.pm.PackageManager
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
+void main() {
+  runApp(const IOSLauncher());
+}
 
-class MainActivity : FlutterActivity() {
+class IOSLauncher extends StatelessWidget {
+  const IOSLauncher({super.key});
 
-    private val CHANNEL = "launcher_channel"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
-            .setMethodCallHandler { call, result ->
-
-                val pm = packageManager
-
-                when (call.method) {
-
-                    "getApps" -> {
-                        val apps = pm.getInstalledApplications(PackageManager.GET_META_DATA)
-
-                        val list = apps.filter {
-                            pm.getLaunchIntentForPackage(it.packageName) != null
-                        }.map {
-                            mapOf(
-                                "label" to pm.getApplicationLabel(it).toString(),
-                                "package" to it.packageName
-                            )
-                        }
-
-                        result.success(list)
-                    }
-
-                    "openApp" -> {
-                        val pkg = call.argument<String>("package")
-                        val intent = pm.getLaunchIntentForPackage(pkg!!)
-                        startActivity(intent)
-                        result.success(true)
-                    }
-                }
-            }
-    }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
 }
